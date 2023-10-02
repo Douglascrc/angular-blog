57bf6c86684701c638fbedfdab3701d38aa430f2
Douglascrc <douglasbuscam@gmail.com>
2023-10-01 22:46:28 -0300

    atualização de conteúdo

---
+++ angular-blog/src/app/data/dataFake.ts
@@ -0,0 +1,20 @@
+export const dataFake = [
+ {
+    "id": 1,
+    "photo":"../../../assets/Pokedex.png" ,
+    "title":"",
+    "description": "Olá"
+ },
+ {
+    "id":2,
+    "photo":"../../../assets/habits.png",
+    "title":"Hábitos",
+    "description":"Novos hábitos"
+ },
+ {
+   "id":3,
+   "photo":"https://ifound.ca/wp-content/uploads/2020/05/ecommerce.jpg",
+   "title":"Projeto E-commerce",
+   "description":"Desenvolvideo em Node.js e React.js"
+ }
+]  
